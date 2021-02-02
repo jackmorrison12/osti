774c7d3d92e15bc8cf06e370b0fc52b82b957b78
jackmorrison <jsm1317@ic.ac.uk>
2021-02-02 00:33:54 +0000

    Adds metadata to profile page, and loading indicator on dynamic pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "../styles/globals.css";
 import { Provider } from "next-auth/client";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/profile.js
+++ pages/profile.js
@@ -2,38 +2,45 @@ import { getSession } from "next-auth/client";
 import Link from "next/link";
 import { connectToDatabase } from "../middleware/mongodb";
 import { ObjectId } from "mongodb";
+import Head from "next/head";
 
 export default function Profile({ user, lastfm }) {
   return (
-    <div className="container mx-auto text-center">
-      <h1 className="text-6xl m-12">Profile Page</h1>
-      <img className="inline" src={user.image} />
-      <p className="m-6 text-xl">
-        Welcome {user.name} - this page can only be seen by a logged in user!
-      </p>
-      {lastfm ? (
-        <p>You're lastfm account is connected as {lastfm}</p>
-      ) : (
-        <a
-          href={
-            "http://www.last.fm/api/auth/?api_key=" +
-            process.env.LASTFM_KEY +
-            "&cb=" +
-            process.env.NEXTAUTH_URL +
-            "/api/auth/lastfm"
-          }
-        >
-          Connect to lastfm
-        </a>
-      )}
+    <>
+      <Head>
+        <title>Profile</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto text-center">
+        <h1 className="text-6xl m-12">Profile Page</h1>
+        <img className="inline" src={user.image} />
+        <p className="m-6 text-xl">
+          Welcome {user.name} - this page can only be seen by a logged in user!
+        </p>
+        {lastfm ? (
+          <p>You're lastfm account is connected as {lastfm}</p>
+        ) : (
+          <a
+            href={
+              "http://www.last.fm/api/auth/?api_key=" +
+              process.env.LASTFM_KEY +
+              "&cb=" +
+              process.env.NEXTAUTH_URL +
+              "/api/auth/lastfm"
+            }
+          >
+            Connect to lastfm
+          </a>
+        )}
 
-      <p className="m-2 text-l">
-        Return to{" "}
-        <Link href="/">
-          <a className="text-blue-500 hover:underline">home</a>
-        </Link>
-      </p>
-    </div>
+        <p className="m-2 text-l">
+          Return to{" "}
+          <Link href="/">
+            <a className="text-blue-500 hover:underline">home</a>
+          </Link>
+        </p>
+      </div>
+    </>
   );
 }
 
