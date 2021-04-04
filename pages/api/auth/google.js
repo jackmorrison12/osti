ed6fbd72376cabe57bc2725ec5b2a1c1000f16d5
jackmorrison <jsm1317@ic.ac.uk>
2021-04-04 23:31:23 +0100

    Store whole google token object when connecting fit api

--- pages/api/auth/google.js
+++ pages/api/auth/google.js
@@ -4,7 +4,6 @@ import { getSession } from "next-auth/client";
 import { google } from "googleapis";
 
 export default async function handler(req, res) {
-
   const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_ID,
     process.env.GOOGLE_SECRET,
@@ -17,11 +16,12 @@ export default async function handler(req, res) {
   const { db } = await connectToDatabase();
   const global_session = await getSession({ req });
 
+  // Store google token object
   await db.collection("users").updateOne(
     { _id: ObjectId(global_session.id) },
     {
       $set: {
-        google_refresh_token: tokens.refresh_token,
+        google_tokens: tokens,
       },
     },
     { upsert: true }
