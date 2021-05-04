a7d42f05e3291324898025086b80edfae64faa78
jackmorrison <jsm1317@ic.ac.uk>
2021-05-04 17:37:59 +0100

    Updates persmission

--- pages/profile.js
+++ pages/profile.js
@@ -136,6 +136,7 @@ export async function getServerSideProps(ctx) {
       "https://www.googleapis.com/auth/fitness.activity.read",
       "https://www.googleapis.com/auth/fitness.heart_rate.read",
       "https://www.googleapis.com/auth/fitness.sleep.read",
+      "https://www.googleapis.com/auth/fitness.location.read",
     ];
 
     url = oauth2Client.generateAuthUrl({
